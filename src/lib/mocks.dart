c6d7eea496a585df83b776b39e50fd4091bdbf91
Mikhail Ushanov <mikhail.ushanov@gmail.com>
2021-09-17 19:51:33 +0300

    4.6 Design

--- src/lib/mocks.dart
+++ src/lib/mocks.dart
@@ -2,24 +2,31 @@ import 'package:places/domain/sight.dart';
 
 final List<Sight> mocks = [
   Sight(
-      'Мост Волны Хендерсона',
+      'Воронежский областной краеведческий музей',
       1.276091,
       103.815448,
       'https://wikipoints.ru/point/525',
       'Henderson Road, Southern Ridges, Bukit Merah, Singapore',
-      'bridge'),
+      'мост'),
   Sight(
       'Дьявольский мост',
       41.620474,
       25.114209,
       'https://en.wikipedia.org/wiki/Dyavolski_most',
-      'Ardino, Bulgaria',
-      'bridge'),
+      'Henderson Road, Southern Ridges, Bukit Merah, Singapore',
+      'мост'),
   Sight(
       'Церковь Святого Георгия',
       12.032248,
       39.041955,
       'https://en.wikipedia.org/wiki/Church_of_Saint_George,_Lalibela',
       'Lalibela, Ethiopia',
-      'church')
+      'церковь'),
+  Sight(
+      'Сказочный мост',
+      24.8256237,
+      106.4961667,
+      'https://www.redbull.com/ru-ru/7-places-discoveries-google-earth',
+      'Ксянрен, Гуанси-Чжуанский автономный район, Китай',
+      'мост')
 ];
